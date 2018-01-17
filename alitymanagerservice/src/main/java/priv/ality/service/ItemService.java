5677f7fcc6db3d10835acc2e1a75e6fc573d206c
zenhobby <877756324@qq.com>
2018-01-17 17:36:14 +0800

    Item List Done.

--- alitymanagerservice/src/main/java/priv/ality/service/ItemService.java
+++ alitymanagerservice/src/main/java/priv/ality/service/ItemService.java
@@ -1,7 +1,11 @@
 package priv.ality.service;
 
 import priv.ality.pojo.TbItem;
+import priv.ality.utils.EasyUIDataGridResult;
+
+import java.util.List;
 
 public interface ItemService {
     TbItem getItemById(Long itemId);
+    EasyUIDataGridResult getItemList(int page, int rows);
 }
--- alitymanagerservice/src/main/java/priv/ality/service/impl/ItemServiceImpl.java
+++ alitymanagerservice/src/main/java/priv/ality/service/impl/ItemServiceImpl.java
@@ -1,10 +1,16 @@
 package priv.ality.service.impl;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import priv.ality.mapper.TbItemMapper;
 import priv.ality.pojo.TbItem;
+import priv.ality.pojo.TbItemExample;
 import priv.ality.service.ItemService;
+import priv.ality.utils.EasyUIDataGridResult;
+
+import java.util.List;
 
 /**
  * 商品查询Service
@@ -17,5 +23,22 @@ public class ItemServiceImpl implements ItemService {
     @Override
     public TbItem getItemById(Long itemId) {
         return itemMapper.selectByPrimaryKey(itemId);
+    }
+
+    @Override
+    public EasyUIDataGridResult getItemList(int page, int rows) {
+        //分页处理
+        PageHelper.startPage(page, rows);
+        //执行查询
+        TbItemExample example = new TbItemExample();
+        List<TbItem> list = itemMapper.selectByExample(example);
+        //取分页信息
+        PageInfo<TbItem> pageInfo = new PageInfo<>(list);
+        //返回处理结果
+        EasyUIDataGridResult result = new EasyUIDataGridResult();
+        result.setTotal(pageInfo.getTotal());
+        result.setRows(list);
+
+        return result;
     }
 }
